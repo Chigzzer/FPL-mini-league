2556c990b981b7372bb4d3922205aa86adb9e9ca
chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-08-22 22:02:34 +0100

    Updated string formatting style

--- terminal.py
+++ terminal.py
@@ -1,15 +1,13 @@
 import data
 
-print('The top gameweek scored: ' + str(data.top_gw_points))
-print('The following players scored the maximum points at each of the following gameweeks')
-for winner in range(len(data.top_gws)):
-    print(str(data.top_gw_players[winner]) + ': ' + str(data.top_gws[winner]))
+print("The top gameweek scored: {} points".format(data.top_gw_points))
+print("The following players scored the maximum points at each of the following gameweeks: {} at {}".format(*data.top_gw_players, *data.top_gws))
+"""for winner in range(len(data.top_gws)):
+    print(str(data.top_gw_players[winner]) + ': ' + str(data.top_gws[winner]))"""
 
 
 print('The top scorers for each month are below:')
 for month in data.months:
     if 'top_scorer' not in month.keys():
         continue
-    print(month['month_name'] + ' Top Scorers who scored: ' + str(month['top_points']) + ' are:')
-    for player in month['top_scorer']:
-        print(player)
+    print("{} Managers of the Month are: {}, who scored {} points".format(month['month_name'], *month['top_scorer'], month['top_points'] ))
