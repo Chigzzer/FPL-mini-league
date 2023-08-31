fc4009f691d11150062bc43a373dd822437c996c
chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-08-31 18:23:40 +0100

    started production of web page

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, flash
+import data
+
+
+#Creating application
+app = Flask(__name__)
+app.config['Debug'] = True
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", top_gw_points = data.top_gw_points, top_gw_players = data.top_gw_players, top_gws = data.top_gws)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
